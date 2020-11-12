b2a678f9fbdd3bb1042ccc1f60be0ca0554a2f36
naiheyouqing <1192257163@qq.com>
2020-11-12 17:48:33 +0800

    commons搭建

--- eureka-yuangou/src/main/java/com/yuangou/eureka/EurekaMain18000.java
+++ eureka-yuangou/src/main/java/com/yuangou/eureka/EurekaMain18000.java
@@ -1,4 +1,4 @@
-package com.yuangou.eurekayuangou;
+package com.yuangou.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
